94aeec037521f93876c7acef1a03ecc766d40cd2
Gary Huong <garyhuong@gmail.com>
2021-03-08 12:21:07 -0500

    Implement 4.5*: helper functions and unit tests step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,7 +4,19 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null;
+
+  const { title, author, url, likes } = blogs.reduce(
+    (favorite, blog) => (blog.likes > favorite.likes ? blog : favorite),
+    blogs[0]
+  );
+
+  return { title, author, url, likes };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
