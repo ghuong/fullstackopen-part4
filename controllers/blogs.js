17450660ced57162332efd76d0ee54dbe3f7987f
Gary Huong <garyhuong@gmail.com>
2021-03-28 11:57:49 -0400

    Implement Exercise 4.22*: bloglist expansion, step10

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,33 +1,28 @@
 const jwt = require("jsonwebtoken");
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
-const User = require("../models/user");
+const userExtractor = require("../utils/middleware").userExtractor;
+const logger = require("../utils/logger");
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
 });
 
-blogsRouter.post("/", async (request, response) => {
+blogsRouter.post("/", userExtractor, async (request, response) => {
   const body = request.body;
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-  if (!request.token || !decodedToken.id) {
-    return response.status(401).json({ error: "token missing or invalid" });
-  }
-
-  const user = await User.findById(decodedToken.id);
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes ?? 0,
-    user: user._id,
+    user: request.user._id,
   });
 
   const savedBlog = await blog.save();
-  user.blogs = user.blogs.concat(blog._id); // Add blog post to user's blogs
-  await user.save();
+  request.user.blogs = request.user.blogs.concat(blog._id); // Add blog post to user's blogs
+  await request.user.save();
 
   return response.status(201).json(savedBlog);
 });
@@ -47,16 +42,11 @@ blogsRouter.put("/:id", async (request, response) => {
   response.json(updatedBlog);
 });
 
-blogsRouter.delete("/:id", async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-  if (!request.token || !decodedToken.id) {
-    return response.status(401).json({ error: "token missing or invalid" });
-  }
-
-  const userId = decodedToken.id;
+blogsRouter.delete("/:id", userExtractor, async (request, response) => {
   const blog = await Blog.findById(request.params.id);
 
-  if (blog.user.toString() !== userId) {
+  if (blog.user.toString() !== request.user._id.toString()) {
+    logger.error("blog.user: ", blog.user.toString(), "user:", request.user._id);
     return response
       .status(401)
       .json({ error: "you are not authorized to delete this blog post" });
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,5 +1,7 @@
+const jwt = require("jsonwebtoken");
 const logger = require("./logger");
 const morgan = require("morgan");
+const User = require("../models/user");
 
 morgan.token("body", (request, response) => {
   if (request.method === "POST") {
@@ -39,6 +41,15 @@ const tokenExtractor = (request, response, next) => {
   next();
 };
 
+const userExtractor = async (request, response, next) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+  request.user = await User.findById(decodedToken.id);
+  next();
+};
+
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message);
 
@@ -61,4 +72,5 @@ module.exports = {
   unknownEndpoint,
   errorHandler,
   tokenExtractor,
+  userExtractor,
 };
